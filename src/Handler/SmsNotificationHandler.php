08dad5923b373186236a5a15d99dc07d3c42b827
Morior <jordi.grados@escapadarural.com>
2022-01-26 14:46:07 +0100

    Configuring symfony messenger

---
+++ src/Handler/SmsNotificationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Handler;
+
+use App\Message\SmsNotification;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class SmsNotificationHandler
+{
+    public function __invoke(SmsNotification $message)
+    {
+    }
+}
