d433f60392d9b89780d807d8381616a65c59b6a7
Morior <jordi.grados@escapadarural.com>
2022-01-26 14:18:50 +0100

    Move symfony directory

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
